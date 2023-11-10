24bf8781bfb941180491e899f3ad814e04a8e196
Pascal Jodlowski <jodlowskipascal@gmail.com>
2023-11-10 17:09:45 +0100

    :bug: Make no access check more specific

--- librus_apix/helpers.py
+++ librus_apix/helpers.py
@@ -3,8 +3,9 @@ from librus_apix.exceptions import TokenError
 import re
 
 def no_access_check(soup: BeautifulSoup):
-    no_access = re.search('Brak dostępu', soup.text)
-    if no_access:
+    pattern = 'Brak dostępu'
+    no_access = soup.select_one("#page h2.inside")
+    if pattern in no_access.get_text():
         raise TokenError("Malformed or expired token.")
     else:
         return soup
