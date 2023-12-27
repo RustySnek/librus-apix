a8adc2819b7877041dda81d4c45f2031c5dcefd2
Pascal Jodlowski <jodlowskipascal@gmail.com>
2023-12-27 10:26:44 +0100

    :technologist: Add a way to add custom endpoints

--- librus_apix/urls.py
+++ librus_apix/urls.py
@@ -1,10 +1,11 @@
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0",
+    "Content-Type": "application/x-www-form-urlencoded",
 }
 BASE_URL = "https://synergia.librus.pl"
 API_URL = "https://api.librus.pl"
 ANNOUNCEMENTS_URL = f"{BASE_URL}/ogloszenia"
-MESSAGE_URL = f"{BASE_URL}/wiadomosci/1/5/"
+MESSAGE_URL = f"{BASE_URL}/wiadomosci/1/5"
 ATTENDANCE_URL = f"{BASE_URL}/przegladaj_nb/szczegoly/"
 SCHEDULE_URL = f"{BASE_URL}/terminarz/"
 HOMEWORK_URL = f"{BASE_URL}/moje_zadania/podglad/"
