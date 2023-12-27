0c5d7352e4db952d89922126d86adbe2aedf5ba1
Pascal Jodlowski <jodlowskipascal@gmail.com>
2023-12-27 10:26:44 +0100

    :technologist: Add a way to add custom endpoints

--- librus_apix/get_token.py
+++ librus_apix/get_token.py
@@ -4,12 +4,24 @@ from requests.models import Response
 from bs4 import BeautifulSoup
 from requests import Session
 from requests.utils import cookiejar_from_dict, dict_from_cookiejar
-from librus_apix.urls import API_URL, BASE_URL, HEADERS
+import librus_apix.urls as urls
 from librus_apix.exceptions import AuthorizationError, MaintananceError
 
 
 class Token:
-    def __init__(self, API_Key: Optional[str] = None):
+    def __init__(
+        self,
+        API_Key: Optional[str] = None,
+        base_url: Optional[str] = None,
+        api_url: Optional[str] = None,
+        announcements_url: Optional[str] = None,
+        message_url: Optional[str] = None,
+        attendance_url: Optional[str] = None,
+        schedule_url: Optional[str] = None,
+        homework_url: Optional[str] = None,
+        info_url: Optional[str] = None,
+        completed_lessons_url: Optional[str] = None,
+    ):
         self._session = Session()
         if not API_Key:
             self.cookies = ""
@@ -19,26 +31,53 @@ class Token:
         cookies = {"DZIENNIKSID": API_Key.split(":")[0]}
         cookies["SDZIENNIKSID"] = API_Key.split(":")[1]
         self.cookies = cookies
+        self.BASE_URL = base_url if base_url else urls.BASE_URL
+        self.API_URL = api_url if api_url else urls.API_URL
+        self.ANNOUNCEMENTS_URL = (
+            announcements_url if announcements_url else urls.ANNOUNCEMENTS_URL
+        )
+        self.MESSAGE_URL = message_url if message_url else urls.MESSAGE_URL
+        self.ATTENDANCE_URL = attendance_url if attendance_url else urls.ATTENDANCE_URL
+        self.SCHEDULE_URL = schedule_url if schedule_url else urls.SCHEDULE_URL
+        self.HOMEWORK_URL = homework_url if homework_url else urls.HOMEWORK_URL
+        self.INFO_URL = info_url if info_url else urls.INFO_URL
+        self.COMPLETED_LESSONS_URL = (
+            completed_lessons_url
+            if completed_lessons_url
+            else urls.COMPLETED_LESSONS_URL
+        )
 
     def post(self, url: str, data: Dict[str, Union[str, int]]) -> Response:
         with self._session as s:
-            s.headers = HEADERS
+            s.headers = urls.HEADERS
             s.cookies = cookiejar_from_dict(self.cookies)
-            response: Response = s.post(url, data)
+            response: Response = s.post(url, json=data)
             return response
 
     def get(self, url: str) -> Response:
         with self._session as s:
-            s.headers = HEADERS
+            s.headers = urls.HEADERS
             s.cookies = cookiejar_from_dict(self.cookies)
             response: Response = s.get(url)
             return response
 
 
-def get_token(username: str, password: str) -> Token:
+def get_token(
+    username: str,
+    password: str,
+    base_url: Optional[str] = None,
+    api_url: str = urls.API_URL,
+    announcements_url: Optional[str] = None,
+    message_url: Optional[str] = None,
+    attendance_url: Optional[str] = None,
+    schedule_url: Optional[str] = None,
+    homework_url: Optional[str] = None,
+    info_url: Optional[str] = None,
+    completed_lessons_url: Optional[str] = None,
+) -> Token:
     with Session() as s:
-        s.headers = HEADERS
-        maint_check = s.get("https://api.librus.pl/")
+        s.headers = urls.HEADERS
+        maint_check = s.get(api_url)
         if maint_check.status_code == 503:
             message_list = maint_check.json().get("Message")
             if not message_list:
@@ -46,18 +85,30 @@ def get_token(username: str, password: str) -> Token:
                 raise MaintananceError("maintenance")
             raise MaintananceError(message_list[0]["description"])
         s.get(
-            API_URL
+            api_url
             + "/OAuth/Authorization?client_id=46&response_type=code&scope=mydata"
         )
         response = s.post(
-            API_URL + "/OAuth/Authorization?client_id=46",
+            api_url + "/OAuth/Authorization?client_id=46",
             data={"action": "login", "login": username, "pass": password},
         )
         if response.json()["status"] == "error":
             raise AuthorizationError(response.json()["errors"][0]["message"])
 
-        res = s.get(API_URL + response.json().get("goTo"))
+        s.get(api_url + response.json().get("goTo"))
+
         cookies = dict_from_cookiejar(s.cookies)
-        token = Token(str(cookies["DZIENNIKSID"] + ":" + cookies["SDZIENNIKSID"]))
+        token = Token(
+            str(cookies["DZIENNIKSID"] + ":" + cookies["SDZIENNIKSID"]),
+            base_url,
+            api_url,
+            announcements_url,
+            message_url,
+            attendance_url,
+            schedule_url,
+            homework_url,
+            info_url,
+            completed_lessons_url,
+        )
 
         return token
