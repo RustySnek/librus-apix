357c2865f5112514f98e62b259e993fbbe61baf9
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-01-02 12:51:18 +0100

    :adhesive_bandage: Return soup if there is no no-access message

--- librus_apix/helpers.py
+++ librus_apix/helpers.py
@@ -1,13 +1,12 @@
 from bs4 import BeautifulSoup
 from librus_apix.exceptions import TokenError, ParseError
-import re
 
 
 def no_access_check(soup: BeautifulSoup):
     pattern = "Brak dostępu"
     no_access = soup.select_one("#page h2.inside")
     if not no_access:
-        raise ParseError("Error when checking page access")
+        return soup
     if pattern in no_access.get_text():
         raise TokenError("Malformed or expired token.")
     else:
