00e6d2b2063140874782c9e642c1f3d2326f2609
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-04-30 12:42:26 +0200

    :white_check_mark: update example

--- examples/send_messages.py
+++ examples/send_messages.py
@@ -1,29 +1,17 @@
-from librus_apix.messages import get_recipients, send_message
+from librus_apix.messages import recipient_groups, get_recipients, send_message
 from librus_apix.get_token import get_token
 
 username = "USERNAME"
 password = "PASSWORD"
 token = get_token(username, password)
 
-"""
-Available recipient groups:
-            "teachers": "nauczyciel"
-            "tutors": "wychowawca"
-            "parent_council": "szkolna_rada_rodzicow"
-            "pedagogue": "pedagog"
-            "admin": "admin"
-            "secretary": "sekretariat"
-"""
+groups: List[str] = recipient_groups(token)
 
-recipients = get_recipients(token, "teachers")
-
-"""
-returns a dictionary {"fullname": "recipient_id"} like {"John Brown": "1234567"}
-"""
+recipients: Dict[str, str] = get_recipients(token, groups[0])
 
 my_recipient = recipient_id["John Brown"]
 my_second_recipient = recipient_id["Barbara Brown"]
-sent = send_message(token, "Message Title", "Message\n content", "teachers", [my_recipient, my_second_recipient])
+sent = send_message(token, "Message Title", "Message\n content", [my_recipient, my_second_recipient])
 
 """
 returns either true or false if status_code was 200
