28934b8dc231c33a21fc6e7397e17be1b0d40411
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-15 19:34:52 +0200

    :sparkles: Add timetable test

---
+++ tests/test_timetable.py
@@ -0,0 +1,23 @@
+from typing import DefaultDict
+import pytest
+from datetime import datetime, timedelta
+from librus_apix.timetable import get_timetable, Period
+
+today = datetime.now()
+
+current_weekday = today.weekday()
+most_recent_monday = today - timedelta(days=current_weekday)
+monday_from_last_month = most_recent_monday - timedelta(weeks=4)
+monday_from_next_month = most_recent_monday + timedelta(weeks=4)
+
+@pytest.mark.parametrize("monday", [
+    most_recent_monday,
+    monday_from_last_month,
+    monday_from_next_month,
+])
+def test_get_timetable(token, monday):
+    timetable = get_timetable(token, monday)
+    assert isinstance(timetable, list)
+    for weekday in timetable:
+        assert isinstance(weekday, list)
+        assert all(isinstance(period, Period) for period in weekday)
