b3554b841013aff8658aec0e7a8bfbca3c779310
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-15 22:07:20 +0200

    ✅ add schedule/info tests

---
+++ tests/test_schedule.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+import pytest
+
+from librus_apix.schedule import get_schedule
+
+now = datetime.now()
+
+@pytest.mark.parametrize("year", [
+    now.year
+])
+@pytest.mark.parametrize("month", [
+    now.month,
+    (now + timedelta(days=31)).month,
+    (now - timedelta(days=31)).month
+])
+def test_get_schedule_with_empty_days(token, year, month):
+    schedule = get_schedule(token, month, year, True)
+    assert isinstance(schedule, dict)
+    assert len(schedule) > 20
+
+@pytest.mark.parametrize("year", [
+    now.year
+])
+@pytest.mark.parametrize("month", [
+    now.month,
+    (now + timedelta(days=31)).month,
+    (now - timedelta(days=31)).month
+])
+def test_non_empty_schedule(token, year, month):
+    schedule = get_schedule(token, month, year, False)
+    assert isinstance(schedule, dict)
+    assert all(len(day) > 0 for day in schedule.values())
