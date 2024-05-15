c0a1ce9a2c652a6b40a7bd0b143dbc9dfbb0ba90
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-15 22:06:51 +0200

    ✅ updoot tests

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,12 +1,26 @@
 import pytest
-from librus_apix.messages import Message, get_recieved, get_sent, get_max_page_number
+from librus_apix.messages import Message, get_recieved, get_sent, get_max_page_number, message_content
 
-def test_get_grades(token):
+def test_get_max_page(token):
     max_page = get_max_page_number(token)
     assert isinstance(max_page, int)
-    recieved = get_recieved(token, max_page)
-    assert isinstance(recieved, list)
-    assert all(isinstance(msg, Message) for msg in recieved)
-    sent = get_sent(token, max_page)
+    assert max_page >= 0
+
+def test_get_sent_messages(token):
+    sent = get_sent(token, 1)
     assert isinstance(sent, list)
     assert all(isinstance(msg, Message) for msg in sent)
+
+@pytest.fixture
+def test_get_recieved_messages(token):
+    recieved = get_recieved(token, 1)
+    assert isinstance(recieved, list)
+    assert all(isinstance(msg, Message) for msg in recieved)
+    return recieved
+
+def test_message_content(test_get_recieved_messages, token):
+    if len(test_get_recieved_messages) == 0:
+        pytest.skip("No messages to check")
+    sample: Message = test_get_recieved_messages[0]
+    content = message_content(token, sample.href)
+    assert isinstance(content, str)
