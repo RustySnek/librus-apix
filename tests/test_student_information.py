64068341065fdfd359547578fef9aaf283456131
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-15 23:19:07 +0200

    :art: Black format

--- tests/test_student_information.py
+++ tests/test_student_information.py
@@ -1,6 +1,7 @@
 import pytest
 from librus_apix.student_information import StudentInformation, get_student_information
 
+
 def test_student_information(token):
     info = get_student_information(token)
     assert isinstance(info, StudentInformation)
