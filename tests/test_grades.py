8d016965b6b4b80677fd59a3b53a535c8e9f9ae5
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-15 18:04:07 +0200

    ✅ Add tests

---
+++ tests/test_grades.py
@@ -0,0 +1,16 @@
+from typing import DefaultDict
+import pytest
+from librus_apix.grades import get_grades
+
+@pytest.mark.parametrize("opt", [
+"all",
+"week",
+"last_login"
+])
+def test_get_grades(token, opt):
+    grades, semester_grades, descriptive_grades  = get_grades(token, opt)
+    assert isinstance(grades, list)
+    assert isinstance(descriptive_grades, list)
+    assert isinstance(semester_grades, DefaultDict)
+    assert all(isinstance(semester, dict) for semester in grades)
+    assert all(isinstance(semester, dict) for semester in descriptive_grades)
